eea42ee53df884fb52e85c9f4f299f58859905df
Eric Anderson <eric.sysmin@gmail.com>
2014-04-15 15:19:43 -0700

    removed the pkg directory

--- pkg/ericsysmin-zabbix-0.5.1/manifests/server/service.pp
+++
@@ -1,8 +0,0 @@
-class zabbix::server::service {
-  service { 'zabbix-server':
-    ensure     => running,
-    hasstatus  => true,
-    hasrestart => true,
-    require    => Package["zabbix-server-${zabbix::server::dbType}"],
-  }
-}
